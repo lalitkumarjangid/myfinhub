86ffcad89fee2d93ecd18067ad43ac8313f383af
LALIT KUMAR JANGID <96244930+lalitkumarjangid@users.noreply.github.com>
2024-03-25 23:24:55 +0530

    Update Navbar.tsx

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -48,7 +48,7 @@ const Navbar: FC<NavbarProps> = ({}) => {
         <div className="flex items-center">
           <Link to="/" className="flex gap-2 justify-center items-center">
             <img src="/assets/logo.png" alt="logo" className="w-[88px] -ml-6" />
-            <span className="mr-4 -ml-4 font-bold text-[clamp(1.95rem, 1.56vw + 1.56rem, 2.81rem)]">
+            <span className="mr-4 -ml-4 font-bold text-[clamp(1.95rem, 1.56vw + 1.56rem, 2.81rem)]  text-lg">
            Finhub
             <br />
            <h6 className="mr-2 -ml-2  text-sm text-gray-500">Developed by Lalit</h6>
